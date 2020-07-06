c24359fd6459c6d0908dc41b34fd768adebe1cb2
Andres Ruiz <andrsruiz@gmail.com>
2020-07-06 13:31:40 -0500

    Add Form

--- client/src/components/CharacterCreator.vue
+++ client/src/components/CharacterCreator.vue
@@ -1,12 +1,36 @@
 <template>
 	<div class="character-creator">
 		<h1 class="title">Character Creator</h1>
+		<section>
+			<b-field label="Name">
+				<b-input v-model="name" maxlength="50"></b-input>
+			</b-field>
+
+			<div class="field is-grouped is-grouped-centered">
+				<b-field label="Profession">
+					<b-select placeholder="Select a Profession">
+						<option v-model="profession" v-for="prof in professions" :key="prof">
+							{{ prof }}
+						</option>
+					</b-select>
+				</b-field>
+			</div>
+		</section>
+		{{ name }}
+		{{ profession }}
 	</div>
 </template>
 
 <script>
 export default {
-	name: "CharacterCreator"
+	name: "CharacterCreator",
+	data(){
+		return{
+			name: '',
+			profession: '',
+			professions: ['Mage', 'Warrior', 'Thief']
+		}
+	}
 };
 </script>
 
