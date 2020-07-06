608c885e4d857ed5023887c7af18584e3d851f61
Andres Ruiz <andrsruiz@gmail.com>
2020-07-06 12:00:18 -0500

    Add style loaders

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,50 @@
 <template>
 	<div id="app">
 		<CharacterView />
+		<CharacterCreator />
 	</div>
 </template>
 
 <script>
 import CharacterView from './components/CharacterView.vue'
+import CharacterCreator from './components/CharacterCreator.vue'
 
 export default {
 	name: 'App',
 	components: {
-		CharacterView
+		CharacterView,
+		CharacterCreator
 	}
 }
 </script>
 
-<style>
-#app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-#nav {
-	padding: 30px;
-}
+<style lang="scss">
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
 
-#nav a {
-	font-weight: bold;
-	color: #2c3e50;
-}
+// Set your colors
+$primary: #92A8D1;
+$primary-invert: findColorInvert($primary);
 
-#nav a.router-link-exact-active {
-	color: #42b983;
-}
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+	"white": ($white, $black),
+	"black": ($black, $white),
+	"light": ($light, $light-invert),
+	"dark": ($dark, $dark-invert),
+	"primary": ($primary, $primary-invert),
+	"info": ($info, $info-invert),
+	"success": ($success, $success-invert),
+	"warning": ($warning, $warning-invert),
+	"danger": ($danger, $danger-invert),
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
 </style>
